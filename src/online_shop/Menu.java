0f4026086cf3e84b19a42e499311123e176367c8
Artem <varty1010@gmail.com>
2021-01-13 19:23:54 +0200

    hw_6

---
+++ src/online_shop/Menu.java
@@ -0,0 +1,15 @@
+package online_shop;
+
+public class Menu {
+	
+	public void print() {
+		
+		System.out.println("1. Sign Up\r\n"
+				+ "2. Sign In / Sign Out \r\n"
+				+ "3. Product Catalog\r\n"
+				+ "4. My Orders\r\n"
+				+ "5. Settings\r\n"
+				+ "6. Customer List\r\n");
+	}
+ 
+} 
